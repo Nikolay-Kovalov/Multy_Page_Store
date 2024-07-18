f0127dfc756d7cd960f2991c2ef4192ecc6c20b5
Nikolay-Kovalov <106543342+Nikolay-Kovalov@users.noreply.github.com>
2024-07-18 21:25:51 +0300

    added changes

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,6 +1,7 @@
 const burgerBtn = document.querySelector('.burger-menu-button');
 const mobileBackdrop = document.querySelector('.mobile-backdrop');
 const mobileModal = document.querySelector('.mobile-modal');
+const closeBtn = document.querySelector('.close-btn-mobile');
 
 
 burgerBtn.addEventListener('click', handleOpenBurger);
@@ -8,4 +9,11 @@ burgerBtn.addEventListener('click', handleOpenBurger);
 function handleOpenBurger(){
 mobileModal.classList.add('burger-active');
 document.body.style.overflow = "hidden";
+}
+
+closeBtn.addEventListener('click', handleCloseBurger);
+
+function  handleCloseBurger(){
+    mobileModal.classList.remove('burger-active');
+    document.body.style.overflow = "visible";  
 }
